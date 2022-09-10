e52bc785ddf800528e33554a31c6750179d2eee7
ruslaan.gilazefwgm <ruslaan.gilazefwgm@gmail.com>
2022-09-10 14:32:05 +0300

    Выполнил 3адачу 1

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задача 1
+        System.out.println("Задача 1");
+        int first = 100;
+        System.out.println("Значение переменной first с типом int равно " + first);
+        byte second = 50;
+        System.out.println("Значение переменной second с типом byte равно " + second);
+        short third = 150;
+        System.out.println("Значение переменной third с типом short равно " + third);
+        long fourth = 300L;
+        System.out.println("Значение переменной fourth с типом long равно " + fourth);
+        float fifth = 3.14F;
+        System.out.println("Значение переменной fifth с типом float равно " + fifth);
+        double sixth = 14.345647;
+        System.out.println("Значение переменной sixth с типом double равно " + sixth);
+
+        // Задача 2
+        System.out.println("Задача 2");
+
+    }
+}
